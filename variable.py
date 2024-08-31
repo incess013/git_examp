3afd971279ae7cafc6d6116a0350a280437e18d0
Princess Rodiel <cssjhnnamae@gmail.com>
2024-08-31 11:28:58 +0800

    First commit

---
+++ variable.py
@@ -0,0 +1,18 @@
+# Python Variable
+
+x = 2
+
+name = "Justine"
+
+# camel case
+nameOfStudent = "Justine"
+
+# snake case
+name_of_student = "Justine"
+
+# Pascal case
+NameOfStudent = "Justine"
+
+
+print(x)
+print(name)
